1ccb285c1d20a3dc8d21160fcfc0f5a774dc0f48
stephen <stephen.lenzi@gmail.com>
2018-08-10 09:20:11 +0100

    implements trial tracking in session and detection of pre/post/habit

--- looming_spots/db/session.py
+++ looming_spots/db/session.py
@@ -36,6 +36,14 @@ class Session(object):
         return os.path.join(PROCESSED_DATA_DIRECTORY, parent_dir, session_dir)
 
     @property
+    def video_path(self):
+        return os.path.join(self.path, 'camera.mp4')
+
+    @property
+    def parent_path(self):
+        return os.path.split(self.path)[0]
+
+    @property
     def mouse_name(self):
         return self.path.split('/')[-2]
 
@@ -43,14 +51,39 @@ class Session(object):
     def loom_paths(self):
         paths = []
         for name in os.listdir(self.path):
+            if name == 'habituation_looms':
+                continue
             loom_folder = os.path.join(self.path, name)
             if os.path.isdir(loom_folder) and 'loom' in name:
                 paths.append(loom_folder)
+        if self.n_looms == 0:
+            print('no looms in {}'.format(self.path))
+            return []
         if len(paths) == 0:
             raise LoomsNotTrackedError(self.path)
         loom_indices = [int(path[-1]) for path in paths]
-        sorted_paths, idx = generic_functions.sort_by(paths, loom_indices,descend=False)
+        sorted_paths, idx = generic_functions.sort_by(paths, loom_indices, descend=False)
         return sorted_paths
+
+    @property
+    def contrast_protocol(self):
+        pd = self.photodiode_trace
+        n_samples = 500
+        pre_start = self.trials[0].sample_number - 600
+        pre_end = pre_start + n_samples
+        post_start = self.trials[0].sample_number + 500
+        post_end = post_start + n_samples
+
+        baseline_before_protocol = np.median(pd[pre_start:pre_end])
+        baseline_during_protocol = np.median(pd[post_start:post_end])
+        if baseline_before_protocol - baseline_during_protocol > 0.01:  # FIXME: hard code
+            return 'gradient'
+        else:
+            return 'constant'
+
+    @property
+    def manual_loom_idx(self):
+        return self.metadata['manual_loom_idx']
 
     @property
     def trials_results(self):
@@ -58,7 +91,15 @@ class Session(object):
 
     @property
     def n_trials(self):
-        return len(self.trials_results)
+        return len(self.trials)
+
+    @property
+    def n_test_trials(self):
+        return len([t for t in self.trials if t.trial_type == 'test'])
+
+    @property
+    def n_habituation_trials(self):
+        return len([t for t in self.trials if t.trial_type == 'habituation'])
 
     def hours(self):
         return self.dt.hour + self.dt.minute/60
@@ -73,28 +114,63 @@ class Session(object):
 
     @property
     def n_looms(self):
-        mtd = experiment_metadata.load_metadata(self.path)
-        loom_idx = mtd['loom_idx']
-        return len(loom_idx)
+        return len(self.loom_idx)
 
     @property
     def reference_frame(self):
         fpath = os.path.join(self.path, 'ref.npy')
-        return np.load(fpath)
+        if os.path.isfile(fpath):
+            return np.load(fpath)
+
+    def get_reference_frame(self, trial_type):
+        fpath = os.path.join(self.path, '{}_ref.npy'.format(trial_type))
+        if os.path.isfile(fpath):
+            return np.load(fpath)
 
     @property
     def trials(self):
         trials = []
-        for loom_path in self.loom_paths:
-            t = Trial(self, loom_path)
+        for i, idx in enumerate(self.loom_idx[::5]):
+            t = LoomTrial(self, directory=self.path, sample_number=idx)
+            t.time_to_first_loom = self.time_to_first_loom()
             trials.append(t)
         return trials
 
     def plot_trials(self):
+        for t in self.trials[0:3]:
+            if t.trial_type == 'test':
+                color = 'r' if t.is_flee() else 'k'
+                plt.plot(t.normalised_x_track, color=color)
+
+    def plot_trials_with_habituation(self, habit_lim=5):
+        fig, axes = plt.subplots(1, 3, figsize=(10, 1))
         for t in self.trials:
-            color = 'r' if t.is_flee() else 'k'
-            plt.plot(t.normalised_x_track, color=color)
-            t.plot_peak_acceleration()
+            if t.trial_type == 'test':
+                plt.sca(axes[1])
+                color = 'r' if t.is_flee() else 'k'
+                plt.plot(t.normalised_x_track, color=color)
+            elif t.trial_type == 'habituation':
+                if habit_lim > 0:
+                    habit_lim -= 1
+                    plt.sca(axes[0])
+                    t.plot_track_on_image(180, -200)
+
+                    plt.sca(axes[2])
+                    #plt.plot(t.smoothed_x_speed, color='k')
+                    #plt.plot(t.smoothed_y_speed, color='b')  #TODO: normalise y
+                    #plt.ylim(-0.035, 0.035)
+                    plt.imshow(self.habituation_speeds(), aspect='auto', vmin=-0.03, vmax=0.03)
+                #t.plot_peak_acceleration()
+        plotting.plot_looms_ax(axes[1])
+        #plotting.plot_looms_ax(axes[2])
+
+    def plot_habituation_reaction_metric(self, color):
+        trials = self.get_trials('habituation')
+        reactions = [max(t.smoothed_x_speed[200:400]) for t in trials]
+        plt.plot(reactions, color=color, linewidth=0.5)
+
+    def get_trials(self, key):
+        return [t for t in self.trials if t.trial_type == key]
 
     @property
     def photodiode_trace(self, raw=False):
@@ -108,13 +184,41 @@ class Session(object):
     def metadata(self):
         return experiment_metadata.load_metadata(self.path)
 
-    @property
+    @cached_property
     def loom_idx(self):
-        return self.metadata['loom_idx']
+        loom_idx_path = os.path.join(self.path, 'loom_starts.npy')
+        if not os.path.isfile(loom_idx_path):
+            _ = photodiode.get_loom_idx_from_raw(self.path, save=True)[0]
+        return np.load(loom_idx_path)
+
+    @property
+    def habituation_loom_idx(self):
+        return photodiode.get_habituation_loom_idx(self.loom_idx)
+
+    @property
+    def test_loom_idx(self):
+        return photodiode.get_manual_looms(self.loom_idx)
 
     @property
     def manual_loom_idx(self):
         return self.metadata['manual_loom_idx']
+
+    @property
+    def habituation_protocol_start(self):
+        return photodiode.get_habituation_start(self.loom_idx)
+
+    def habituation_speeds(self):
+        all_speeds = []
+        for t in self.get_trials('habituation'):
+            all_speeds.append(t.smoothed_x_speed)
+        return all_speeds
+
+    @property
+    def contains_habituation(self):
+        return photodiode.contains_habituation(self.loom_idx)
+
+    def test_loom_classification(self):
+        assert len(self.habituation_loom_idx) + len(self.test_loom_idx) == int(len(self.loom_idx)/5)  # FIXME: hard code
 
     @property
     def context(self):
@@ -122,13 +226,42 @@ class Session(object):
 
     @property
     def protocol(self):
-        return experiment_metadata.get_session_label_from_loom_idx(self.loom_idx)
+        return experiment_metadata.get_session_label_from_loom_idx(self.loom_idx, self.n_habituation_looms)
 
     @property
     def grid_location(self):
         mtd = experiment_metadata.load_metadata(self.path)
         grid_location = mtd['grid_location']
         return grid_location
+
+    def histology(self, histology_name='injection_site.png'):
+        histology_path = os.path.join(self.parent_path, histology_name)
+        if os.path.isfile(histology_path):
+            return scipy.ndimage.imread(histology_path)
+        return False
+
+    def time_to_first_loom(self):
+        if len(self.loom_idx) > 0:
+            if 'time_of_mouse_entry' in self.metadata:
+                return (int(self.loom_idx[0]) - int(self.metadata['time_of_mouse_entry']))/30/60
+            return int(self.loom_idx[0])/30/60
+
+    def plot_all_separate_plots(self):
+        fig, axes = plt.subplots(self.n_trials)
+        for t, ax in zip(self.trials, axes):
+            plt.axes(ax)
+            t.plot_track()
+
+    def extract_all_trials(self):
+        for t in self.trials:
+            t.extract_video()
+
+    def track_all_trials(self):
+        #try:
+        for t in self.trials:
+            t.track_trial()
+        #except NoReferenceFrameError as e:
+        #    viewer.Viewer(self.path)
 
     def __lt__(self, other):
         return self.dt < other.dt
@@ -140,3 +273,4 @@ class Session(object):
 class LoomsNotTrackedError(Exception):
     def __init__(self, msg):
         print('no loom folder paths, please check you have tracked this session: {}'.format(msg))
+
