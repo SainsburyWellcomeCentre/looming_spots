669e91d8ee37e493e8324d9271a8c2cb067c7340
stephen <stephen.lenzi@gmail.com>
2018-09-21 11:46:15 +0100

    updates load sessions and experiment functions

--- looming_spots/db/load.py
+++ looming_spots/db/load.py
@@ -9,25 +9,35 @@ from looming_spots.db.paths import PROCESSED_DATA_DIRECTORY
 from looming_spots.db import session_group, experiment
 
 
+class NotExtractedError(Exception):
+    pass
+
+
 def load_sessions(mouse_id):
     sessions_path = os.path.join(PROCESSED_DATA_DIRECTORY, mouse_id)
     session_list = []
-    for s in os.listdir(sessions_path):
-        if not looming_spots.util.generic_functions.is_datetime(s):
-            continue
-        date = datetime.strptime(s, '%Y%m%d_%H_%M_%S')
-        s = session.Session(dt=date, mouse_id=mouse_id)
-        session_list.append(s)
-    return sorted(session_list)
+    if os.path.isdir(sessions_path):
+        for s in os.listdir(sessions_path):
+            if not looming_spots.util.generic_functions.is_datetime(s):
+                continue
+            date = datetime.strptime(s, '%Y%m%d_%H_%M_%S')
+            s = session.Session(dt=date, mouse_id=mouse_id)
+            session_list.append(s)
+        return sorted(session_list)
+    raise(NotExtractedError)
 
 
-def load_experiment_from_groups(groups, group_labels):
+def load_experiment_from_groups(groups, group_labels, test_type='post'):
     sgs = []
     for ids, label in zip(groups, group_labels):
         sessions = []
         for mid in ids:
             ms = session_group.MouseSessionGroup(mid)
-            s = ms.nth_post_test(0)
+            print(ms.mouse_id)
+            if test_type == 'pre':
+                s = ms.nth_pre_test(0)
+            elif test_type == 'post':
+                s = ms.nth_post_test(0)
             sessions.append(s)
         sgs.append(session_group.SessionGroup(sessions, group_key=label))
     exp = experiment.Experiment(sgs)
