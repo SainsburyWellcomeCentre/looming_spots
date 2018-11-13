66ea1d102008196480e3f19939ce4681b62e1cb8
stephen <stephen.lenzi@gmail.com>
2018-11-13 13:28:59 +0000

    contingency plotting with stats

--- looming_spots/analysis/stats.py
+++ looming_spots/analysis/stats.py
@@ -1,11 +1,12 @@
 import itertools
 import numpy as np
-import matplotlib.pyplot as plt
 import scipy.stats
 from looming_spots.analysis import tracks
 import itertools as it
 import collections
 import pandas as pd
+import matplotlib.pyplot as plt
+
 # from rpy2.robjects import pandas2ri
 # from rpy2.robjects.packages import importr
 # pandas2ri.activate()
@@ -28,8 +29,8 @@ def get_all_group_contingencies(session_dictionary):
 
     for label, sessions in session_dictionary.items():
         contingencies_dict.setdefault(label, get_contingencies(sessions))
-    contingencies_df = pd.DataFrame.from_dict(contingencies_dict)
-    contingencies_df.set_index([['flees', 'non-flees']])
+    contingencies_df = convert_contingency_dict(contingencies_dict)
+
     return contingencies_df
 
 
@@ -39,7 +40,15 @@ def plot_contingencies_from_session_dict(session_dictionary):
 
 
 def plot_contingency_df(contingency_df):
-    contingency_df.T.plot(kind='bar', stacked=True, colors=['r', 'k'])
+    ax = contingency_df.T.plot(kind='bar', stacked=True, color=['r', 'k'])
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+
+def convert_contingency_dict(contingencies_dict, labels=['flees', 'non-flees']):
+    contingencies_df = pd.DataFrame.from_dict(contingencies_dict)
+    contingencies_df = contingencies_df.set_index([labels])
+    return contingencies_df
 
 
 def compare_all_groups_fisher(contingency_table):  # TODO: clean this
@@ -121,7 +130,7 @@ def get_all_p_values(contingency_table_dict):
     for pairing in all_combinations:
         keys = list(pairing.keys())
         results = list(pairing.values())
-        new_key = '{}_{}'.format(keys[0], keys[1])
+        new_key = '{}_vs_{}'.format(keys[0], keys[1])
         print(results)
         _, p_value = scipy.stats.fisher_exact([results[0], results[1]])
         p_values[new_key] = p_value
@@ -141,4 +150,32 @@ def get_sorted(flees, days):
     sorted_flees = [flee for flee, _ in sorted(zip(flees, days))]
     return sorted_flees, sorted_days
 
+
+def get_bar_location_from_key(key, labels):
+    bar_x = [label.get_position() for label in labels if label.get_text() == key]
+    if isinstance(bar_x, list):
+        return bar_x[0]
+    return bar_x
+
+
+def plot_contingencies_with_stats(contingencies):
+    stats_results = get_all_p_values(contingencies)
+    c_df = convert_contingency_dict(contingencies)
+    plot_contingency_df(c_df)
+    ax = plt.gca()
+    labels = list(ax.get_xticklabels())
+
+    for i, result in enumerate(stats_results.items()):
+        group_a, group_b = result[0].split('_vs_')
+        pos_a = get_bar_location_from_key(group_a, labels)
+        pos_b = get_bar_location_from_key(group_b, labels)
+
+        n_points = 5
+        x_line = np.linspace(min(pos_a[0], pos_b[0]), max(pos_a[0], pos_b[0]), n_points)
+
+        line_y_pos = max([sum(v) for v in contingencies.values()]) + 5 * (i+1)
+        stat_loc = np.mean(x_line)
+
+        plt.plot(x_line, [line_y_pos] * n_points, linewidth=1, color='k')
+        plt.text(stat_loc, line_y_pos + 1, result[1])
 
