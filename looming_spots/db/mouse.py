049d82cc9025fb584e0241f06a50cb919bb96534
stephen <stephen.lenzi@gmail.com>
2018-12-13 10:03:36 +0000

    updates mouse

--- looming_spots/db/mouse.py
+++ looming_spots/db/mouse.py
@@ -4,11 +4,10 @@ import datetime
 class Mouse(object):
 
     def __init__(self, mouse_id=None, strain='C57BL/6',
-                 entry_date=datetime.date.today(), dob=None, gender='male', tag=None):
-
+                 entry_date=datetime.date.today(), dob=None, sex='male', tag=None):
         self.id = mouse_id
         self.strain = strain
         self.dob = dob
         self.entry_date = entry_date
-        self.gender = gender
+        self.sex = sex
         self.tag = tag
