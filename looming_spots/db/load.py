02eabed9f3571ec4016a1e9fd507c912dc4122c7
stephen <stephen.lenzi@gmail.com>
2018-12-13 09:53:26 +0000

    removes deprecated function in load

--- looming_spots/db/load.py
+++ looming_spots/db/load.py
@@ -3,25 +3,26 @@ from datetime import datetime
 
 import looming_spots.preprocess.extract_looms
 import looming_spots.util.generic_functions
-from looming_spots.db import session
+from looming_spots.db import session_io
 
 from looming_spots.db.paths import PROCESSED_DATA_DIRECTORY
 from looming_spots.db import session_group, experiment
 import warnings
+
 
 class NotExtractedError(Exception):
     pass
 
 
 def load_sessions(mouse_id):
-    sessions_path = os.path.join(PROCESSED_DATA_DIRECTORY, mouse_id)
+    mouse_directory = os.path.join(PROCESSED_DATA_DIRECTORY, mouse_id)
     session_list = []
-    if os.path.isdir(sessions_path):
-        for s in os.listdir(sessions_path):
+    if os.path.isdir(mouse_directory):
+        for s in os.listdir(mouse_directory):
             if not looming_spots.util.generic_functions.is_datetime(s):
                 continue
             date = datetime.strptime(s, '%Y%m%d_%H_%M_%S')
-            s = session.Session(dt=date, mouse_id=mouse_id)
+            s = session_io.Session(dt=date, mouse_id=mouse_id)
             session_list.append(s)
         return sorted(session_list)
     warnings.warn('the mouse: {} has not been processed'.format(mouse_id))
