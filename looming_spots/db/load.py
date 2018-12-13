9b18b1f6630a01e4b56b53bcaed0b90c5fd6e940
stephen <stephen.lenzi@gmail.com>
2018-12-13 09:57:03 +0000

    removes experiment loading from load

--- looming_spots/db/load.py
+++ looming_spots/db/load.py
@@ -6,7 +6,6 @@ import looming_spots.util.generic_functions
 from looming_spots.db import session_io
 
 from looming_spots.db.paths import PROCESSED_DATA_DIRECTORY
-from looming_spots.db import session_group, experiment
 import warnings
 
 
@@ -26,21 +25,3 @@ def load_sessions(mouse_id):
             session_list.append(s)
         return sorted(session_list)
     warnings.warn('the mouse: {} has not been processed'.format(mouse_id))
-    #raise(NotExtractedError('{}'.format(mouse_id)))
-
-
-def load_experiment_from_groups(groups, group_labels, test_type='post'):
-    sgs = []
-    for ids, label in zip(groups, group_labels):
-        sessions = []
-        for mid in ids:
-            ms = session_group.MouseSessionGroup(mid)
-            print(ms.mouse_id)
-            if test_type == 'pre':
-                s = ms.nth_pre_test(0)
-            elif test_type == 'post':
-                s = ms.nth_post_test(0)
-            sessions.append(s)
-        sgs.append(session_group.SessionGroup(sessions, group_key=label))
-    exp = experiment.Experiment(sgs)
-    return exp
