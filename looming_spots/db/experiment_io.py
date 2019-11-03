8312fc1daa993a3cf8d7b96201af12b6e2dca802
stephen <stephen.lenzi@gmail.com>
2019-11-03 16:27:12 +0000

    updates experiment io todo remove

--- looming_spots/db/experiment_io.py
+++ looming_spots/db/experiment_io.py
@@ -4,7 +4,7 @@ from cached_property import cached_property
 from looming_spots.db import load
 
 
-class ExperimentIo(object):
+class ExperimentIo(object):  #TODO: merge with ltg
     def __init__(self, mouse_id):
         self.mouse_id = mouse_id
 
@@ -14,8 +14,7 @@ class ExperimentIo(object):
     @cached_property
     def sessions(self):  # TODO: weakref
         unlinked_sessions = load.load_sessions(self.mouse_id)
-        singly_linked_trials = []
-        doubly_linked_sessions = []
+        singly_linked_trials, doubly_linked_sessions = [], []
 
         for i, (s_current, s_next) in enumerate(
             zip(unlinked_sessions[0:-1], unlinked_sessions[1:])
@@ -31,3 +30,27 @@ class ExperimentIo(object):
             s_next.set_previous_session(s_next, s_current)
             doubly_linked_sessions.append(s_next)
         return doubly_linked_sessions
+
+    @cached_property
+    def linked_trials(
+        self
+    ):
+        unlinked_trials = self.data('trials')
+        singly_linked_trials = []
+        doubly_linked_trials = []
+
+        for i, (t_current, t_next) in enumerate(
+            zip(unlinked_trials[0:-1], unlinked_trials[1:])
+        ):
+            t_current.set_next_trial(t_current, t_next)
+            singly_linked_trials.append(t_current)
+        singly_linked_trials.append(unlinked_trials[-1])
+
+        doubly_linked_trials.append(singly_linked_trials[0])
+        for i, (t_current, t_next) in enumerate(
+            zip(singly_linked_trials[0:-1], singly_linked_trials[1:])
+        ):
+            t_next.set_previous_trial(t_next, t_current)
+            doubly_linked_trials.append(t_next)
+
+        return doubly_linked_trials
