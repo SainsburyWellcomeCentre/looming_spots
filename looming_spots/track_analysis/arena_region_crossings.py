c96e8c19c17e1ddfe7fb8dcfa5dd69235922c868
stephen <stephen.lenzi@gmail.com>
2019-11-06 17:09:00 +0000

    adds docs and renames

--- looming_spots/track_analysis/arena_region_crossings.py
+++ looming_spots/track_analysis/arena_region_crossings.py
@@ -25,6 +25,14 @@ into the others for analysis of responses to aversive stimuli.
 
 
 def is_entry(sample, place_of_entry, place_of_exit):
+    """
+    checks if the mouse has moved from the place of exit to the place of entry at a specific sample
+
+    :param sample: sample number to check
+    :param bool_array place_of_entry: box region that the mouse is entering (1 if in region, 0 if not)
+    :param bool_array place_of_exit: box region that the mouse is leaving (1 if in region, 0 if not)
+    :return:
+    """
     if place_of_entry[sample + 1]:
         if place_of_exit[sample] == 1:
             return True
@@ -32,8 +40,17 @@ def is_entry(sample, place_of_entry, place_of_exit):
 
 
 def get_next_entry(start, place_of_entry, place_of_exit):
-    tz_transitions = np.where(np.diff(place_of_entry))[0]
-    for t in tz_transitions:
+    """
+    returns the next timepoint in samples that the mouse has moved from the place of exit to the place of entry
+
+    :param start: starting point in the track in samples
+    :param place_of_entry: box region that the mouse is entering
+    :param place_of_exit: box region that the mouse is leaving
+    :return:
+    """
+
+    region_crossings = np.where(np.diff(place_of_entry))[0]
+    for t in region_crossings:
         if t < start:
             continue
         if is_entry(t, place_of_entry, place_of_exit):
@@ -52,42 +69,16 @@ def get_next_entry_from_track(
     :param start:
     :return:
     """
-    shelter_front = normalised_shelter_front(context)
+    shelter_boundary = normalised_shelter_front(context)
     tz_boundary = 0.6
     positions = {
-        "shelter": normalised_x_track < shelter_front,
+        "shelter": normalised_x_track < shelter_boundary,
         "middle": np.logical_and(
             (tz_boundary > normalised_x_track),
-            (normalised_x_track > shelter_front),
+            (normalised_x_track > shelter_boundary),
         ),
         "tz": normalised_x_track > tz_boundary,
     }
     return get_next_entry(
         start, positions[place_of_entry_key], positions[place_of_exit_key]
     )
-
-
-def is_tz_entry(sample, tz, middle):
-    """
-
-    :param sample:
-    :param tz: boolean array of tz occupance
-    :param middle: boolean array of middle region occupance
-    :return bool:
-    """
-    return is_entry(sample, tz, middle)
-
-
-def is_shelter_entry(sample, shelter, middle):
-    """
-
-    :param sample:
-    :param tz: boolean array of tz occupance
-    :param middle: boolean array of middle region occupance
-    :return bool:
-    """
-    return is_entry(sample, shelter, middle)
-
-
-def get_next_tz_entry(start, tz, middle):
-    get_next_entry(start, tz, middle)
