ee62f1f0df2cd03bbf88c0731485938e18062469
Stephen Lenzi <s.lenzi@ucl.ac.uk>
2020-11-10 15:29:06 +0100

    shows average latency

--- looming_spots/thesis_figure_plots/signal_latency_plot.py
+++ looming_spots/thesis_figure_plots/signal_latency_plot.py
@@ -70,7 +70,7 @@ def calculate_theoretical_escape_threshold(mtg):
         #plot integral at latency
         if latency is not None:
             print(f'latency: {latency}')
-            plt.axhline(t.integral_downsampled()[int(latency)], 'b')
+            plt.axhline(t.integral_downsampled()[int(latency)], color='b')
         plt.plot(t.integral_downsampled())
         plt.xlim([0, 600])
 
