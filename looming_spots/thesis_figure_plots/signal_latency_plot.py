318ca4e9d22581aaac9df4253e24abce0abe2089
Stephen Lenzi <s.lenzi@ucl.ac.uk>
2020-12-07 14:07:29 +0000

    scale factor bug

--- looming_spots/thesis_figure_plots/signal_latency_plot.py
+++ looming_spots/thesis_figure_plots/signal_latency_plot.py
@@ -530,6 +530,7 @@ def plot_pre_test_trials_with_predicted_values(mtg):
                 scale_factor = float(first_thresh) / float(val_at_latency)
                 plt.plot(t.integral_downsampled()[:int(latency)], color=color)
                 plt.plot(t.integral_downsampled()[:int(latency)]*scale_factor, linestyle='dotted', color=color)
+                print(f'FIRST: {first_thresh}, TRIAL: {val_at_latency}, SCALE_FACTOR: {scale_factor} NEW MAX: {t.integral_downsampled()[int(latency)]*scale_factor}')
     fname = f'integral_at_latency_with_prediction_{mtg.mouse_id}'
     fig.savefig(f'/home/slenzi/thesis_latency_plots/{fname}.eps', format='eps')
 
